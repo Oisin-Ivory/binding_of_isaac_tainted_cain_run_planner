53e6f6a3f438a4419274a606397a2941099c2cb9
Oisín Ivory <47002132+Oisin-Ivory@users.noreply.github.com>
2022-02-13 14:42:45 +0000

    Added search for Select item

--- Android-Studio/app/src/main/java/org/wit/boitcrp/fragments/ItemSelectFragment.kt
+++ Android-Studio/app/src/main/java/org/wit/boitcrp/fragments/ItemSelectFragment.kt
@@ -10,7 +10,9 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.setFragmentResult
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.wit.boitcrp.R
+import org.wit.boitcrp.adapters.ItemAdapter
 import org.wit.boitcrp.adapters.ItemAdapterSelection
+import org.wit.boitcrp.databinding.FragmentItemListBinding
 import org.wit.boitcrp.databinding.FragmentItemSelectBinding
 import org.wit.boitcrp.databinding.FragmentRunListBinding
 import org.wit.boitcrp.main.MainApp
@@ -23,6 +25,7 @@ class ItemSelectFragment : Fragment() {
     private lateinit var binding: FragmentItemSelectBinding
     private lateinit var items: List<Item>
     private var remove: Boolean = true
+    private lateinit var displayItems: List<Item>
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -41,7 +44,7 @@ class ItemSelectFragment : Fragment() {
             items = bundle.getParcelableArray("item_list")!!.toList() as List<Item>
             remove = true
         }
-
+        displayItems = items
     }
 
     override fun onCreateView(
@@ -59,7 +62,7 @@ class ItemSelectFragment : Fragment() {
         else
             binding.recyclerView.adapter = ItemAdapterSelection(app.items.findAll(), this)
 
-
+        setButtonListener(binding)
         return root
     }
 
@@ -72,6 +75,42 @@ class ItemSelectFragment : Fragment() {
             setFragmentResult("item_to_add", bundleOf("item" to result))
         }
         getActivity()?.onBackPressed();
+    }
+
+    fun setButtonListener(layout: FragmentItemSelectBinding) {
+        layout.searchbtn.setOnClickListener {
+            searchTerms()
+        }
+    }
+
+    fun searchTerms(){
+        val terms = binding.searchField.text.split(",")
+        displayItems = searchItems(terms)
+        loadItems()
+    }
+
+    fun searchItems(searchTerms : List<String>):List<Item>{
+        val returnList = emptyList<Item>().toMutableList()
+
+        for (item in items){
+            var addToList = true;
+            for(term in searchTerms){
+                if(!item.toString().lowercase().contains(term.lowercase())){
+                    addToList = false
+                }
+            }
+            if(addToList) returnList.add(item)
+        }
+        return returnList
+    }
+
+    private fun loadItems() {
+        showItems(displayItems)
+    }
+
+    fun showItems (items: List<Item>) {
+        binding.recyclerView.adapter = ItemAdapterSelection(items, this)
+        binding.recyclerView.adapter?.notifyDataSetChanged()
     }
 
 
